7f0b9b91ff7ac6bd0358ded92cfda36edb6651d5
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-05-27 15:25:55 -0400

    added account cs and tests

--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -6,8 +6,16 @@ namespace BankingApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Good Morning, Banker!");
+            Console.WriteLine("Welcome to the BankingApp!\n" );
+
+            Console.WriteLine("Select the service you'd like to use from below: ");
+            Console.WriteLine("1: Check Account Balance");
+            Console.WriteLine("2: Withdraw $10");
+            Console.WriteLine("3: Deposit $50");
+            Console.WriteLine("4: Exit");
+
+            string accountChoice = Console.ReadLine();
+
         }
     }
 }
